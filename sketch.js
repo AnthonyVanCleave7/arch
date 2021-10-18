bd13db6106283ded99f070371d3319cfbbb31060
AnthonyVanCleave7 <87826768+AnthonyVanCleave7@users.noreply.github.com>
2021-10-17 20:08:19 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,73 +1,163 @@
 const Engine = Matter.Engine;
-const Render = Matter.Render;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-const Body = Matter.Body;
-const Composites = Matter.Composites;
-const Composite = Matter.Composite;
 
-let engine;
-let world;
-var ground, bridge;
-var leftWall, rightWall;
-var jointPoint;
-var jointLink;
+var engine, world;
+var canvas,baseimage,playerimage;
+var palyer, playerBase, playerArcher;
+var playerArrows = [];
+var numberOfArrows = 10;
+var board1, board2;
 
-var stones = [];
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
+
   engine = Engine.create();
   world = engine.world;
-  frameRate(80);
+  angleMode(DEGREES);
 
-  ground = new Base(0, height - 10, width * 2, 20, "#795548", true);
-  leftWall = new Base(300, height / 2 + 50, 600, 100, "#8d6e63", true);
-  rightWall = new Base(width - 300, height / 2 + 50, 600, 100, "#8d6e63", true);
+  var options = {
+    isStatic: true
+  };
 
-  bridge = new Base(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
 
-  bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
 
-  bridge = new Base(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Bridge(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.position.y - 112,
+    120,
+    120
+  );
 
-  bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Bridge(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);
-
-  
-  Matter.Composite.add(bridge.body, jointPoint);
-
-  Matter.Composite.add(jointPoint);
-  
-  Matter.Composite.add(jointPoint, bridge.body);
-  
-  Matter.Composite.add(bridge.body);
-
-
-  jointLink = new Link(bridge, jointPoint);
-
-  for (var i = 0; i <= 8; i++) {
-    var x = random(width / 2 - 200, width / 2 + 300);
-    var y = random(-10, 140);
-    var stone = new Stone(x, y, 80, 80);
-    stones.push(stone);
-  }
+  board1 = new Board(width - 300, 330, 50, 200);
+  board2 = new Board(width - 550, height - 300, 50, 200);
 }
 
 function draw() {
-  background(51);
+  background(backgroundImg );
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+
   Engine.update(engine);
 
-  ground.show();
-  bridge.show();
-  leftWall.show();
-  rightWall.show();
+  playerArcher.display();
 
-  for (var stone of stones) {
-    stone.show();
+  board1.display();
+  board2.display();
+
+  for (var i = 0; i < playerArrows.length; i++) {
+    if (playerArrows[i] !== undefined) {
+      playerArrows[i].display();
+
+      //with distance formula
+      d1 = dist(playerArrows[i].body.position.x,playerArrows[i].body.position.y, board1.body.position.x,board1.body.position.y)
+      if(d1<=100)
+      {
+        console.log("collision");
+      }
+
+      var board1Collision = Matter.SAT.collides(
+        board1.body,
+        playerArrows[i].body
+      );
+
+      var board2Collision = Matter.SAT.collides(
+        board2.body,
+        playerArrows[i].body
+      );
+
+      if (board1Collision.collided || board2Collision.collided) {
+        console.log("yes");
+      }
+
+      if(board1Collision.collided || board2Collision.collided){
+        score +=5;
+      }
+
+      //[optional code to add trajectory of arrow]
+      
+      // var posX = playerArrows[i].body.position.x;
+      // var posY = playerArrows[i].body.position.y;
+
+      // if (posX > width || posY > height) {
+      //   if (!playerArrows[i].isRemoved) {
+      //     playerArrows[i].remove(i);
+      //   } else {
+      //     playerArrows[i].trajectory = [];
+      //   }
+      // }
+    }
+  }
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+  // Arrow Count
+  fill("#FFFF");
+  textAlign("center");
+  textSize(30);
+  text("Remaining Arrows : " + numberOfArrows, 200, 100);
+
+  if (numberOfArrows == 0) {
+    console.log("arrow bucket is empty")
   }
 }
+
+function keyPressed() {
+  if (keyCode === 32) {
+    if (numberOfArrows > 0) {
+      var posX = playerArcher.body.position.x;
+      var posY = playerArcher.body.position.y;
+      var angle = playerArcher.body.angle;
+
+      var arrow = new PlayerArrow(posX, posY, 100, 20, angle);
+
+      arrow.trajectory = [];
+      Matter.Body.setAngle(arrow.body, angle);
+      playerArrows.push(arrow);
+      numberOfArrows -= 1;
+    }
+  }
+}
+
+function keyReleased() {
+  if (keyCode === 32) {
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
+}
+
+function gameOver(){
+  swal(
+{
+  title: 'Game Over!!!',
+  text: "Thanks for playing!",
+  imageUrl:
+  "https://raw.githubbusercontent.com/vishalgaddam873/PiratesInvision/main/assets/board/png",
+  imageSize: "150x150",
+  confirmButtonText: "Play Again"
+},
+function(isConfirm) {
+  if(isConfirm){
+    location.reload();
+  }
+}
+  )
+}
+
